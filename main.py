30af396ade20f8ff14721835d53ce7e6a6c0e18a
yym77 <492114475@qq.com>
2026-03-30 18:35:57 +0800

    v1

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ from seven_ai_layers_robotics.generating import (
     CharacterisationReportPipeline,
     EdgeReportPipeline
 )
-# from seven_ai_layers_robotics.recipeQA import CorpusGenerator
-# import asyncio
+from seven_ai_layers_robotics.recipeQA import CorpusGenerator
+import asyncio
 # from seven_ai_layers_robotics.reasoning import PerovskiteReportGenerator
 
 # from seven_ai_layers_robotics.evaluation import MIRecipeEvaluator
@@ -23,27 +23,27 @@ if __name__ == "__main__":
     # pipeline2.run_full_process("data3000")
 
     # pipeline3 = CharacterizationDataPipeline()
-    # pipeline3.run_full_process()
-    pipeline = VariableReportPipeline()
-    success = pipeline.run(steps='all', rebuild_knowledge=True, verbose=True)
-    print("VariableReportPipeline", success)
-# 
-    # Run characterisation report generation pipeline
-    pipeline2 = CharacterisationReportPipeline()
-    success = pipeline2.run(report_type='all', verbose=True)
-    print("CharacterisationReportPipeline", success)
+#     # pipeline3.run_full_process()
+#     pipeline = VariableReportPipeline()
+#     success = pipeline.run(steps='all', rebuild_knowledge=True, verbose=True)
+#     print("VariableReportPipeline", success)
+# # 
+#     # Run characterisation report generation pipeline
+#     pipeline2 = CharacterisationReportPipeline()
+#     success = pipeline2.run(report_type='all', verbose=True)
+#     print("CharacterisationReportPipeline", success)
 
-    # Run edge report generation pipeline
-    pipeline3 = EdgeReportPipeline()
-    success = pipeline3.run(steps='all', verbose=True)
-    print("EdgeReportPipeline", success)
+#     # Run edge report generation pipeline
+#     pipeline3 = EdgeReportPipeline()
+#     success = pipeline3.run(steps='all', verbose=True)
+#     print("EdgeReportPipeline", success)
 
     # Use asyncio.run() to execute async function
 
     
     # Asynchronously call generate_all_async() to generate all corpora
-    # generator = CorpusGenerator()
-    # result = asyncio.run(generator.generate_all_async())
+    generator = CorpusGenerator()
+    result = asyncio.run(generator.generate_all_async())
 
 
     # generator = PerovskiteReportGenerator.from_config()
